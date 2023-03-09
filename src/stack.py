84d8511a86b6d21bd51c4b90b52d3dca97c238c5
WRXer <king_311@mail.ru>
2023-03-09 10:17:53 +0400

    Написаны функции добавления и удаления стека

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,8 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
+        self.stackk = []
 
     def push(self, data):
         """
@@ -23,7 +25,11 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        node = Node(data,self.top)
+        self.stackk.append(data)
+        if self.top:
+            node.next_node = self.top
+        self.top = node
 
     def pop(self):
         """
@@ -31,4 +37,4 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        return self.stackk.pop()

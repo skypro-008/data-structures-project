058ba05be2adae39df264e4fb7930f6268a4f33f
sergey <szaprudnov@hotmail.com>
2023-03-14 11:30:47 +0300

    add foo

--- src/stack.py
+++ src/stack.py
@@ -4,10 +4,10 @@ class Node:
     def __init__(self, data, next_node):
         """
         Конструктор класса Node
+
         :param data: данные, которые будут храниться в узле
         """
-        self.data = data
-        self.next_node = next_node
+        pass
 
 
 class Stack:
@@ -15,23 +15,20 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        self.top = None
+        pass
 
     def push(self, data):
         """
         Метод для добавления элемента на вершину стека
+
         :param data: данные, которые будут добавлены на вершину стека
         """
-        node = Node(data, self.top)
-        self.top = node
+        pass
 
     def pop(self):
         """
         Метод для удаления элемента с вершины стека и его возвращения
+
         :return: данные удаленного элемента
         """
-        node = self.top
-        if not node:
-            return None
-        self.top = node.next_node
-        return node.data
+        pass

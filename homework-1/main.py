b6385d5577e22ea7a96e31b794c76801be1e9757
Ilia Perfilov <ind.climb@gmail.com>
2023-03-15 15:39:09 +0300

    main.py was modified

--- homework-1/main.py
+++ homework-1/main.py
@@ -12,8 +12,13 @@ if __name__ == '__main__':
     stack.push('data1')
     stack.push('data2')
     stack.push('data3')
+
     print(stack.top.data)  # data3
     print(stack.top.next_node.data)  # data2
     print(stack.top.next_node.next_node.data)  # data1
     print(stack.top.next_node.next_node.next_node)  # None
-    print(stack.top.next_node.next_node.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
+    #print(stack.top.next_node.next_node.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
+    print(stack.pop())
+    print(stack.top.data)
+    print(stack.top.next_node.data)  # data2
+    print(stack.top.next_node.next_node.data)  # data1

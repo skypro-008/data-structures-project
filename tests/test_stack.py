5054aba5e0f7f71b059558d479f5b0e686f938d3
Ralina Latypova <Latypovaralina99@gmail.com>
2023-03-17 23:04:22 -0700

    Task 3

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import *
+
+
+class TestStack(unittest.TestCase):
+    def test_push(self):
+        node_test = Node('test1', None)
+        stack_test = Stack()
+        stack_test.push(node_test)
+        self.assertEqual(node_test, stack_test.top.data)

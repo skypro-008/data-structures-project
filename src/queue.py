890a761e868b9b9ad8a36f98ad01283dde29f0d3
Ilia Perfilov <ind.climb@gmail.com>
2023-03-20 17:01:35 +0300

    queue.py was modified -new methods add (enqueue and __str__)

--- src/queue.py
+++ src/queue.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,10 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head: Node | None = None
+        self.tail: Node | None = None
+        self.size: int = 0
+
 
     def enqueue(self, data):
         """
@@ -23,8 +27,12 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
-
+        if self.head is None:
+            self.head = self.tail = Node(data)
+        else:
+            self.tail.next_node = Node(data)
+            self.tail = Node(data)
+        self.size += 1
     def dequeue(self):
         """
         Метод для удаления элемента из очереди и его возвращения
@@ -35,4 +43,12 @@ class Queue:
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        deque_list = list()
+        node = self.head
+        if node is None:
+            return f''
+        while node is not None:
+            deque_list.append(node.data)
+            node = node.next_node
+        deque_list.append(self.tail.data)
+        return '\n'.join(deque_list)

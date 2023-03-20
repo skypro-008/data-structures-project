2052544653e869f7d52383426ea4f1f10ff4978e
Ilia Perfilov <ind.climb@gmail.com>
2023-03-20 17:23:43 +0300

    test_queue.py was modified - tests for new method added

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,27 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Queue
+
+
+class TestQueue(unittest.TestCase):
+
+    def test_str_empty(self):
+        self.assertEqual(str(Queue()), '')
+
+    def setUp(self):
+        self.queue = Queue()
+        self.queue.enqueue('data1')
+        self.queue.enqueue('data2')
+        self.queue.enqueue('data3')
+
+    def test_enqueue(self):
+        self.assertEqual(self.queue.head.data, 'data1')
+        self.assertEqual(self.queue.head.next_node.data, 'data2')
+        self.assertEqual(self.queue.tail.data, 'data3')
+        self.assertIs(self.queue.tail.next_node, None)
+
+        with self.assertRaises(AttributeError):
+            self.queue.tail.next_node.data
+
+    def test_str(self):
+        self.assertEqual(str(self.queue), "data1\ndata2\ndata3")

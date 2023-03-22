e8690b33ea7d2c66fe6b2f6b2255571cb5bd15be
sergey <szaprudnov@hotmail.com>
2023-03-22 17:19:56 +0300

    add tests Node & Queue

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,14 +1,20 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
 import unittest
-from src.stack import Node
+from src.stack import Node, Stack
 
 
 class Teststack(unittest.TestCase):
 
-    def test_stack(self):
+    def test_node(self):
         first = Node(5, None)
         self.assertEqual(first.data, 5)
+        self.assertEqual(first.back_node, None)
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_stack(self):
+        second = Stack()
+        self.assertEqual(second.top, None)
+        second.push('data1')
+        self.assertEqual(isinstance(second.top, Node), True)
+        self.assertEqual(second.pop(), 'data1')
+        self.assertEqual(second.pop(), None)
+        self.assertEqual(str(second), 'None')

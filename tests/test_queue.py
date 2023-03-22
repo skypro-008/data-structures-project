71d9df0fbdcb3dc2edf38831dfed85cc35ffb8a2
sergey <szaprudnov@hotmail.com>
2023-03-22 17:11:41 +0300

    add tests Node & Queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Node, Queue
+
+class Teststack(unittest.TestCase):
+
+    def test_node(self):
+        """Тестирование ноды"""
+        first = Node(5, None)
+        self.assertEqual(first.data, 5)
+        self.assertEqual(first.next_node, None)
+
+    def test_queue(self):
+        """Тестирование класса Queue"""
+        second = Queue()
+        three = Queue()
+        second.enqueue('data1')
+        second.enqueue('data2')
+        second.enqueue('data3')
+        assert str(second) == "data1\ndata2\ndata3"
+        assert str(three) == ""

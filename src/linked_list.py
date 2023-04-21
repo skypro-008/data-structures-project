c96e09b3c18a425c1ed85cf33746d52598892efe
Sandi_Navoiy <santes.au@gmail.com>
2023-04-21 19:17:33 +0300

    5 -е задание, начало похода

--- src/linked_list.py
+++ src/linked_list.py
@@ -10,14 +10,30 @@ class Node:
 
 class LinkedList:
     """Класс для односвязного списка"""
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""

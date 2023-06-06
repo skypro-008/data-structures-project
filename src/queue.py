8fcceb232fac7cf72cfd06cc9a3a442670a848f6
Aleksandr Charushin <charushinas@gmail.com>
2023-06-06 20:22:08 +0300

    Make queue.py, classes Node, Queue

--- src/queue.py
+++ src/queue.py
@@ -1,13 +1,10 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
-        """
-        Конструктор класса Node
-
-        :param data: данные, которые будут храниться в узле
-        """
-        pass
+    def __init__(self, data,):
+        """Конструктор класса Node"""
+        self.data = data
+        self.next_node = None
 
 
 class Queue:
@@ -15,15 +12,27 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
+
+    def __str__(self):
+        """Магический метод для строкового представления объекта"""
+        current_node = self.head
+        queue_str = ""
+        while current_node is not None:
+            queue_str += str(current_node.data) + "\n"
+            current_node = current_node.next_node
+        return queue_str.strip()
 
     def enqueue(self, data):
-        """
-        Метод для добавления элемента в очередь
-
-        :param data: данные, которые будут добавлены в очередь
-        """
-        pass
+        """Метод для добавления элемента в очередь"""
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def dequeue(self):
         """
@@ -31,8 +40,4 @@ class Queue:
 
         :return: данные удаленного элемента
         """
-        pass
-
-    def __str__(self):
-        """Магический метод для строкового представления объекта"""
         pass

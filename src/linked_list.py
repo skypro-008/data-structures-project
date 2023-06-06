b0a56245c4d344b0dc3728482a61a861f2560ad9
AnnaKolmychek <a.kolmychek@gmail.com>
2023-06-07 01:27:43 +0300

    add new class LinkedList()

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,30 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data, next_node):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        self.head = Node(data, self.head)
+        if self.tail is None:
+            self.tail = self.head
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_tail = Node(data, None)
+        if self.tail is not None:
+            self.tail.next_node = new_tail
+        else:
+            self.head = new_tail
+        self.tail = new_tail
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -26,4 +38,4 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.lstrip()

84a662b54798eb271c3889f85b8837834b3a0456
Aliaksandr <sigai.aleksandr@mail.ru>
2023-07-08 22:40:22 +0300

    hm 1

--- src/stack.py
+++ src/stack.py
@@ -1,34 +1,32 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
+
+
+
 
 
 class Stack:
     """Класс для стека"""
 
     def __init__(self):
-        """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
-        """
-        Метод для добавления элемента на вершину стека
-
-        :param data: данные, которые будут добавлены на вершину стека
-        """
-        pass
+        new_node = Node(data, self.top)
+        self.top = new_node
 
     def pop(self):
-        """
-        Метод для удаления элемента с вершины стека и его возвращения
+        data = self.top.data
+        self.top = self.top.next_node
+        return data
 
-        :return: данные удаленного элемента
-        """
-        pass
+
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,32 @@
+import unittest
+from src.stack import Node, Stack
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+
+class StackTest(unittest.TestCase):
+    stack = Stack()
+    def test_push(self):
+      stack = Stack()
+      stack.push('data1')
+      stack.push('data2')
+      stack.push('data3')
+      self.assertEqual(stack.top.data, 'data3')
+      self.assertEqual(stack.top.next_node.data, 'data2')
+      self.assertEqual(stack.top.next_node.next_node.data, 'data1')
+
+    def test_pop(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        stack.push('data3')
+        data = stack.pop()
+        self.assertEqual(data, 'data3')
+        self.assertEqual(stack.top.data, 'data2')
+        data = stack.pop()
+        self.assertEqual(data, 'data2')
+        self.assertEqual(stack.top.data, 'data1')
+        data = stack.pop()
+        self.assertEqual(data, 'data1')
+
+
+
+

873aaa856e24f7f56abafe13008e896014bdefd2
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-07-21 14:03:23 +0200

    Wrote a test for node

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,11 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+import pytest
+from src import queue
+
+
+def test_node():
+    n2 = queue.Node(10, None)
+    n1 = queue.Node("hello", n2)
+
+    assert n1.data == "hello"
+    assert n2.data == 10
+    assert n1.next_node == n2

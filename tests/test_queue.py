bdd6a22b70d1cec87ffecc782316b60274ed5133
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-07-21 14:12:23 +0200

    Wrote a test for constructor and enqueue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -9,3 +9,21 @@ def test_node():
     assert n1.data == "hello"
     assert n2.data == 10
     assert n1.next_node == n2
+
+
+def test_constructor():
+    queue_test = queue.Queue()
+    assert queue_test.head is None
+    assert queue_test.tail is None
+
+
+def test_enqueue():
+    queue_test = queue.Queue()
+    queue_test.enqueue('data1')
+    queue_test.enqueue('data2')
+    queue_test.enqueue('data3')
+
+    assert queue_test.head.data == 'data1'
+    assert queue_test.head.next_node.data == 'data2'
+    assert queue_test.tail.data == 'data3'
+    assert queue_test.tail.next_node is None

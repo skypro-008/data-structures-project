22a54fd2fe27fa7cd4c4a4f820a53f73d954c0c7
exetch <gassemark@gmail.com>
2023-07-28 20:15:12 +0300

    LinkedList implemented, tests added

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,37 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +41,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,34 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.ll = LinkedList()
+
+    def test_empty_list(self):
+        self.assertEqual(str(self.ll), "None")
+
+    def test_insert_beginning(self):
+        self.ll.insert_beginning({'id': 1})
+        self.assertEqual(str(self.ll), "{'id': 1} -> None")
+
+        self.ll.insert_beginning({'id': 0})
+        self.assertEqual(str(self.ll), "{'id': 0} -> {'id': 1} -> None")
+
+    def test_insert_at_end(self):
+        self.ll.insert_at_end({'id': 1})
+        self.assertEqual(str(self.ll), "{'id': 1} -> None")
+
+        self.ll.insert_at_end({'id': 2})
+        self.assertEqual(str(self.ll), "{'id': 1} -> {'id': 2} -> None")
+
+    def test_insert_beginning_and_end(self):
+        self.ll.insert_beginning({'id': 1})
+        self.ll.insert_at_end({'id': 2})
+        self.assertEqual(str(self.ll), "{'id': 1} -> {'id': 2} -> None")
+
+        self.ll.insert_beginning({'id': 0})
+        self.ll.insert_at_end({'id': 3})
+        self.assertEqual(str(self.ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")

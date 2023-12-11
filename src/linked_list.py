d81469a8b742a7c0f1723826c093ff477acf964c
vavil <vavilon164@yandex.ru>
2023-12-11 20:10:56 +0300

    Done homework1

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,37 @@
+from typing import Any
+
+
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data: Any):
+        self.data = data
+        self.next = None
 
 
 class LinkedList:
     """Класс для односвязного списка"""
+    head = None
+    top = None
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        if self.head is None:
+            self.head = Node(data)
+
+        else:
+            node = Node(data)
+            node.next = self.head
+            self.head = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        if self.top is None:
+            self.top = Node(data)
+
+        else:
+            node = Node(data)
+            self.top.next = node
+            self.top = node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,15 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +17,7 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
@@ -23,7 +25,13 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        if self.top is None:
+            self.top = Node(data)
+
+        else:
+            node = Node(data)
+            node.next_node = self.top
+            self.top = node
 
     def pop(self):
         """
@@ -31,4 +39,7 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        data = self.top.data
+        self.top = self.top.next_node
+
+        return data
